be5fad31a9e7db08b462fec6050be83da8df1cb9
mbdilaver <mbdilaver@gmail.com>
2019-02-19 13:45:01 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/weather-icons.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class WeatherConsole extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render() {
+        return (
+            <div className="weather-console">
+                {/*<CardGraphTab />*/}
+                <CardListTab />
+            </div>
+        );
+    }
+}
+
+class CardListTab extends React.Component {
+
+    render() {
+        return (
+            <div className="card-list-tab">
+                <WeatherCard />
+                <WeatherCard />
+                <WeatherCard />
+            </div>
+        );
+    }
+}
+
+class WeatherCard extends React.Component {
+
+    render() {
+        const degreeSign = "\u2103";
+        const weatherInfo = {
+            "day-temp": 20,
+            "night-temp": 17,
+        };
+        return (
+            <div className="card">
+                <div className="card-title label">12:00</div>
+
+                <div className="card-icon">
+                    <i className="wi wi-day-sunny card-icon"></i>
+                </div>
+
+                <div className="temp-pair">
+                    <div className="day-temp label">{weatherInfo["day-temp"] + "\u00B0"}</div>
+                    <div className="night-temp label">{weatherInfo["night-temp"] + "\u00B0"}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <WeatherConsole />,
+    document.getElementById('root')
+);
