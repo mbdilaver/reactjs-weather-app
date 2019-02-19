c46f23778dce019ff31a2beae4217a9c2f3128e8
mbdilaver <mbdilaver@gmail.com>
2019-02-19 14:34:23 +0300

    Static version

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './css/weather-icons.min.css';
 
+
 class WeatherConsole extends React.Component {
 
     render() {
+        const weatherList= this.props.weatherList;
+
         return (
             <div className="weather-console">
                 {/*<CardGraphTab />*/}
-                <CardListTab />
+                <CardListTab weatherList={weatherList}/>
             </div>
         );
     }
@@ -18,11 +21,13 @@ class WeatherConsole extends React.Component {
 class CardListTab extends React.Component {
 
     render() {
+        const weatherList = this.props.weatherList;
+
         return (
             <div className="card-list-tab">
-                <WeatherCard />
-                <WeatherCard />
-                <WeatherCard />
+                {weatherList.map((weather) =>
+                    <WeatherCard weather={weather} />
+                )}
             </div>
         );
     }
@@ -31,29 +36,58 @@ class CardListTab extends React.Component {
 class WeatherCard extends React.Component {
 
     render() {
-        const degreeSign = "\u2103";
-        const weatherInfo = {
-            "day-temp": 20,
-            "night-temp": 17,
-        };
+        const weather = this.props.weather;
         return (
             <div className="card">
-                <div className="card-title label">12:00</div>
+                <div className="card-title label">{weather.time}</div>
 
                 <div className="card-icon">
-                    <i className="wi wi-day-sunny card-icon"></i>
+                    <i className={"wi " + weather.icon}></i>
                 </div>
 
                 <div className="temp-pair">
-                    <div className="day-temp label">{weatherInfo["day-temp"] + "\u00B0"}</div>
-                    <div className="night-temp label">{weatherInfo["night-temp"] + "\u00B0"}</div>
+                    <div className="day-temp label">{weather.dayTemp + "\u00B0"}</div>
+                    <div className="night-temp label">{weather.nightTemp + "\u00B0"}</div>
                 </div>
             </div>
         );
     }
 }
 
+const weatherList = [
+    {
+        time: "Mon",
+        dayTemp: 20,
+        nightTemp: 17,
+        icon: "wi-day-sunny"
+    },
+    {
+        time: "Tue",
+        dayTemp: 19,
+        nightTemp: 15,
+        icon: "wi-day-cloudy"
+    },
+    {
+        time: "Wed",
+        dayTemp: 18,
+        nightTemp: 14,
+        icon: "wi-day-sunny"
+    },
+    {
+        time: "Thu",
+        dayTemp: 15,
+        nightTemp: 11,
+        icon: "wi-day-rain"
+    },
+    {
+        time: "Sun",
+        dayTemp: 3,
+        nightTemp: -1,
+        icon: "wi-snow"
+    },
+];
+
 ReactDOM.render(
-    <WeatherConsole />,
+    <WeatherConsole weatherList={weatherList}/>,
     document.getElementById('root')
 );
