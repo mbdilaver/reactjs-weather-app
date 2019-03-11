c9d6597f478e240d0a945ee1cabca9bc2cb6f172
mbdilaver <mbdilaver@gmail.com>
2019-03-11 17:59:51 +0300

    Updates

---
+++ precache-manifest.fa8ce806ba842fa8b412ff0ff11592de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15daa814310235d5c773",
+    "url": "/reactjs-weather-app/static/js/runtime~main.15daa814.js"
+  },
+  {
+    "revision": "388926e6562c46368946",
+    "url": "/reactjs-weather-app/static/js/main.388926e6.chunk.js"
+  },
+  {
+    "revision": "396fd941a843f13896e8",
+    "url": "/reactjs-weather-app/static/js/2.396fd941.chunk.js"
+  },
+  {
+    "revision": "388926e6562c46368946",
+    "url": "/reactjs-weather-app/static/css/main.47f384a1.chunk.css"
+  },
+  {
+    "revision": "5face26dade0c97d5bc915b32a6edfe0",
+    "url": "/reactjs-weather-app/index.html"
+  }
+];
